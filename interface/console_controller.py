02d718768945489026fac70b01ad5ca579325023
Skalker-god <nazar010308@gmail.com>
2026-03-13 21:38:52 +0200

    Enable multiple bookings in ConsoleController
    Added a loop to allow multiple room bookings until the user decides to stop.

--- interface/console_controller.py
+++ interface/console_controller.py
@@ -4,12 +4,22 @@ class ConsoleController:
         self.use_case = use_case
 
     def run(self):
-        room = input("Room ID: ")
-        start = int(input("Start time: "))
-        end = int(input("End time: "))
 
-        try:
-            booking = self.use_case.execute(room, start, end)
-            print("Booking created!")
-        except Exception as e:
-            print("Error:", e)
+        while True:
+            print("\n--- Room Booking ---")
+
+            room = input("Room ID: ")
+            start = int(input("Start time: "))
+            end = int(input("End time: "))
+
+            try:
+                booking = self.use_case.execute(room, start, end)
+                print("Booking created!")
+
+            except Exception as e:
+                print("Error:", e)
+
+            again = input("Create another booking? (y/n): ")
+
+            if again.lower() != "y":
+                break
